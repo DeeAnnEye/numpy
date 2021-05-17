13fcf66c47dd75e13deee76b817d3403c0fa53a7
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-17 16:13:35 +0530

    NumPy op_dtypes

--- numpy-arrays.py
+++ numpy-arrays.py
@@ -101,3 +101,8 @@ for x in l:
 nd = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[4,5,7]]])
 for x in np.nditer(nd):
     print(x)
+
+# change datatype on iteration
+adt = np.array([1,2,3,4,5])
+for x in np.nditer(adt, flags=['buffered'], op_dtypes='S'):
+    print(x)
